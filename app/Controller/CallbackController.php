d530363bdfb93de3944a580e1112c3196c6c9c60
takpy <hasegawa.takuya.terry@gmail.com>
2017-01-14 17:57:08 +0900

    Refactor CallbackController

--- app/Controller/CallbackController.php
+++ app/Controller/CallbackController.php
@@ -4,34 +4,39 @@ App::uses('AppController', 'Controller');
 
 class CallbackController extends AppController {
 
-	public function index() {
-		$this->autoRender = false;
-		$this->response->type('json');
+        public function index() {
+                $this->autoRender = false;
+                $this->response->type('json');
+                $events = $this->request->input('json_decode', true);
+                $replyMessage = json_encode(
+                        [
+                                'replyToken' => Hash::get($events, 'events.0.replyToken'),
+                                'messages' => [
+                                        [
+                                                'type' => Hash::get($events, 'events.0.message.type'),
+                                                'text' => Hash::get($events, 'events.0.message.text')
+                                        ]
+                                ]
+                        ]
+                );
+                $output = $this->__reply($replyMessage);
+                $this->log($output, 'debug');
+                return $this->response->statusCode(200);
+        }
 
-		$headers = [
-			"Content-Type: application/json; charset=UTF-8",
-			"Authorization: Bearer <TOKEN>"
-		];
+        private function __getHeaders() {
+                return  [
+                        "Content-Type: application/json; charset=UTF-8",
+                        "Authorization: Bearer <<AccessToken>>"
+                ];
+        }
 
-		$events = $this->request->input('json_decode', true);
 
-		$tmp =  [
-				'replyToken' => Hash::get($events, 'events.0.replyToken'),
-				'messages' => [
-					 [
-						'type' => Hash::get($events, 'events.0.message.type'),
-						'text' => Hash::get($events, 'events.0.message.type')
-					]
-				]
-		];
-		$replyMessage = json_encode($tmp);
-
-		$curl = curl_init('https://api.line.me/v2/bot/message/reply');
-		curl_setopt($curl, CURLOPT_POST, true);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, $replyMessage);
-		$output = curl_exec($curl);
-		$this->log($output, 'debug');
-		return $this->response->statusCode(200);
-	}
+	 private function __reply($replyMessage) {
+                $curl = curl_init('https://api.line.me/v2/bot/message/reply');
+                curl_setopt($curl, CURLOPT_POST, true);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, $this->__getHeaders());
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $replyMessage);
+                return curl_exec($curl);
+        }
 }
