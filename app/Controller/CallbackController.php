94b444ec40cacd83bd7e38913c2c498ee75d1fa3
takpy <hasegawa.takuya.terry@gmail.com>
2017-01-15 10:53:43 +0900

    Add morphological analysis

--- app/Controller/CallbackController.php
+++ app/Controller/CallbackController.php
@@ -4,34 +4,63 @@ App::uses('AppController', 'Controller');
 
 class CallbackController extends AppController {
 
-	public function index() {
-		$this->autoRender = false;
-		$this->response->type('json');
+        public function index() {
+                $this->autoRender = false;
+                $this->response->type('json');
+                $events = $this->request->input('json_decode', true);
+		$result = $this->__parseEvents($events);
+                $replyMessage = json_encode(
+                        [
+                                'replyToken' => Hash::get($events, 'events.0.replyToken'),
+                                'messages' => [
+                                        [
+                                                'type' => 'text',
+                                                'text' => $result["type"] . ':'  . $result['value']
+                                        ]
+                                ]
+                        ]
+                );
+                $output = $this->__reply($replyMessage);
+                $this->log($output);
+                return $this->response->statusCode(200);
+        }
 
-		$headers = [
-			"Content-Type: application/json; charset=UTF-8",
-			"Authorization: Bearer <TOKEN>"
-		];
+	private function __parseEvents($events) {
+		if (Hash::get($events, 'events.0.message.type') == 'location') {
+			$type = 'location';
+			$value = Hash::get($events, 'events.0.message.address');
+		} else {
+			$type = 'others';
+			$value = Hash::get($events, 'events.0.message.text');
+			$this->__isContainArea(Hash::get($events, 'events.0.message.text'));
+		}
 
-		$events = $this->request->input('json_decode', true);
-
-		$tmp =  [
-				'replyToken' => Hash::get($events, 'events.0.replyToken'),
-				'messages' => [
-					 [
-						'type' => Hash::get($events, 'events.0.message.type'),
-						'text' => Hash::get($events, 'events.0.message.type')
-					]
-				]
-		];
-		$replyMessage = json_encode($tmp);
-
-		$curl = curl_init('https://api.line.me/v2/bot/message/reply');
-		curl_setopt($curl, CURLOPT_POST, true);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, $replyMessage);
-		$output = curl_exec($curl);
-		$this->log($output, 'debug');
-		return $this->response->statusCode(200);
+		return [ 'type' => $type, 'value' => $value ];
 	}
+
+	private function __isContainArea($text) {
+		$options = array('-d', '/usr/local/lib/mecab/dic/ipadic/');
+		$mecab = new MeCab_Tagger($options);
+		$nodes = $mecab->parseToNode($text);
+		foreach ($nodes as $n) {
+			$this->log($n->getSurface(), 'debug');
+			if (strpos($n->getFeature(), '地域') !== false){
+ 			}
+		}
+	}
+
+	 private function __reply($replyMessage) {
+                $curl = curl_init('https://api.line.me/v2/bot/message/reply');
+                curl_setopt($curl, CURLOPT_POST, true);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, $this->__getHeaders());
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $replyMessage);
+                return curl_exec($curl);
+        }
+
+	private function __getHeaders() {
+                return  [
+                        "Content-Type: application/json; charset=UTF-8",
+                        "Authorization: Bearer <<TOKEN>>"
+		];
+        }
 }
