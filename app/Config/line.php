4c8c8123e2478067bc7e235a3f87fcdd0ef799bf
zein12 <jena.zein@gmail.com>
2017-03-21 06:00:56 +0700

    Update line.php

--- app/Config/line.php
+++ app/Config/line.php
@@ -1,5 +1,5 @@
 <?php
 $config['line'] = [
-	'channelSecret' => '339f2777a4ba46562612ce04d196f414',
-	'accessToken' => 'f3z+ceDnpxhy8wbJs79HlPKkq0EkqNJ7TcPEQKYHzePPBx7i1xOQASGXP0WVPuWjdtLPHSAJjoSptx6YXpp4h5aprNozKBV9cGpC5juQ3thCmutlqgiM5KuUUCFdKmvYEt3SKa61zLZv5eQXvmfG0QdB04t89/1O/w1cDnyilFU='
+	'channelSecret' => 'a6b4b1a80d9f25eb0a719fc92cef7d86',
+	'accessToken' => '3/cEBpOR0mjAMUtnHKrSrx3N6FnMVNPYfXBIwMO6HNGaljxuxTxZz2fGrmZYFwqfV3dvAWMa7FEGrmOONfbZ7or1wxYgpjbtFMS0Mkk+RftjvYSrUpThxAHGiivf2M662z2zM5P8BSKby0dJiBG3GQdB04t89/1O/w1cDnyilFU='
 ];
